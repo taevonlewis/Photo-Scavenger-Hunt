967c72e1c1609559cbd0bad3ab0d165c401bec21
lewist13 <lewistpro@gmail.com>
2023-04-01 14:40:05 -0400

    Initial Commit

---
+++ Photo Scavenger Hunt/Photo_Scavenger_HuntApp.swift
@@ -0,0 +1,17 @@
+//
+//  Photo_Scavenger_HuntApp.swift
+//  Photo Scavenger Hunt
+//
+//  Created by TaeVon Lewis on 4/1/23.
+//
+
+import SwiftUI
+
+@main
+struct Photo_Scavenger_HuntApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
