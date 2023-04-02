e44318265fd90b3c841ba1737f4cec5205ea53c0
lewist13 <lewistpro@gmail.com>
2023-04-02 01:10:51 -0400

    added mock data

--- Photo Scavenger Hunt/Models/Task.swift
+++ Photo Scavenger Hunt/Models/Task.swift
@@ -28,3 +28,16 @@ struct Task {
         self.imageLocation = location
     }
 }
+
+extension Task {
+    static var mockedTasks: [Task] {
+        return [
+            Task(title: "Show light novels",
+                 description: "Show my friends all of my light novels."),
+            Task(title: "Take a pic of my new shoes",
+                 description: "My mom asked for a picture of the new shoes I bought."),
+            Task(title: "Turn on the candle wax machine",
+                 description: "Make the room smell nice!")
+        ]
+    }
+}
