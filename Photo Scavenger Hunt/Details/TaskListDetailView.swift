0f481822eec6435f5e746a8bfc7b1e7db6a07295
lewist13 <lewistpro@gmail.com>
2023-04-02 14:30:15 -0400

    modified task completion state and photo library request

---
+++ Photo Scavenger Hunt/Details/TaskListDetailView.swift
@@ -0,0 +1,79 @@
+//
+//  TaskListDetailView.swift
+//  Photo Scavenger Hunt
+//
+//  Created by TaeVon Lewis on 4/2/23.
+//
+
+import SwiftUI
+import PhotosUI
+
+struct TaskListDetailView: View {
+    @ObservedObject var task: Task
+    @State private var showingPhotoPicker = false
+    @State private var selectedImage: UIImage?
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            HStack {
+                Image(systemName: task.isComplete ? "circle.fill" : "circle")
+                    .font(.headline)
+                    .foregroundColor(task.isComplete ? .green : .gray)
+                Text(task.isComplete ? "Complete" : "Incomplete")
+                    .font(.headline)
+                    .foregroundColor(task.isComplete ? .green : .gray)
+            }
+            VStack(alignment: .leading, spacing: 5) {
+                Text(task.title)
+                    .font(.title2)
+                Text(task.description)
+                    .font(.body)
+            }
+            if let image = task.image {
+                Image(uiImage: UIImage(cgImage: image))
+                    .resizable()
+                    .scaledToFit()
+            } else {
+                Button(action: {
+                    PHPhotoLibrary.requestAuthorization { status in
+                        DispatchQueue.main.async {
+                            if status == .authorized {
+                                showingPhotoPicker = true
+                            }
+                        }
+                    }
+                }, label: {
+                    Text("Attach Photo")
+                        .foregroundColor(.white)
+                        .font(.body)
+                        .frame(maxWidth: .infinity)
+                        .padding(.init(top: 7, leading: 0, bottom: 7, trailing: 0))
+                        .background(RoundedRectangle(cornerRadius: 4))
+                }
+                )}
+        }
+        .sheet(isPresented: $showingPhotoPicker) {
+            PhotoPicker(image: Binding(get: {
+                task.image.map { UIImage(cgImage: $0) }
+            }, set: { image in
+                if let image = image,
+                   let cgImage = image.cgImage {
+                    task.set(cgImage, with: CLLocation())
+                    task.isComplete = true
+                } else {
+                    task.image = nil
+                    task.isComplete = false
+                }
+            })) 
+        }
+        .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .topLeading)
+        .padding(.init(top: 10, leading: 25, bottom: 10, trailing: 25))
+    }
+}
+
+struct TaskListDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        TaskListDetailView(task: Task(title: "Show light novels",
+                               description: "Show my friends all of my light novels."))
+    }
+}
